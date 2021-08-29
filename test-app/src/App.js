9429de3e47c057f8e79724353c74f9a3e2f78a37
seven.duan <seven.j.duan@gmail.com>
2021-08-30 00:59:13 +1000

    antd study

---
+++ test-app/src/App.js
@@ -0,0 +1,14 @@
+// import logo from "./logo.svg";
+// import "./App.css";
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      APP
+      <Button type="primary">Click</Button>
+    </div>
+  );
+}
+
+export default App;
