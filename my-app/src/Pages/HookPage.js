ed53c59e6e467d8bf8e8c7b51f29f71660a52722
seven.duan <seven.j.duan@gmail.com>
2021-08-30 21:29:12 +1000

    custome hook

--- my-app/src/Pages/HookPage.js
+++ my-app/src/Pages/HookPage.js
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from "react";
 
 export default function HookPage(props) {
   const [count, setCount] = useState(0);
-  const [date, setDate] = useState(new Date());
+
+  // cannot do
+  //   if (count) {
+  //     const [num, setNum] = useState(0);
+  //   }
   useEffect(() => {
     console.log("effect"); //sy-log //did mount did update
 
@@ -10,19 +14,28 @@ export default function HookPage(props) {
     document.title = `click ${count} times`;
   }, [count]);
 
+  return (
+    <div>
+      <h3>HookPage</h3>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>add</button>
+      <p>time: {useClock().toLocaleTimeString()}</p>
+    </div>
+  );
+}
+// cannot do
+// function getNum() {
+//   const [num, setNum] = useState(0);
+// }
+
+//Custom Hook starts with use
+function useClock() {
+  const [date, setDate] = useState(new Date());
   useEffect(() => {
     //did Mount
     const timer = setInterval(() => setDate(new Date()), 1000);
     //will unmount
     return () => clearInterval(timer);
   }, []);
-
-  return (
-    <div>
-      <h3>HookPage</h3>
-      <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>add</button>
-      <p>time: {date.toLocaleTimeString()}</p>
-    </div>
-  );
+  return date;
 }
